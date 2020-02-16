766880444275b366ed00c765f0590012d4bad3ac
Oles <ruboken@gmail.com>
2020-02-16 19:59:26 +0200

    Initial commit

---
+++ scripts/sliders.js
@@ -0,0 +1,7 @@
+$('.slider').slick({
+    autoplay: true,
+    // autoplaySpeed: 5000,
+    arrows:true,
+    prevArrow: '<div class="slickArrow slickPrev"><i class="fa fa-angle-left"></i></div>',
+    nextArrow: '<div class="slickArrow slickNext"><i class="fa fa-angle-right"></i></div>',
+});
