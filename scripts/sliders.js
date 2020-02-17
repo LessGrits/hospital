f2e5de4adfc4096dba2cc16bb35a5f17f25cd777
Oles <ruboken@gmail.com>
2020-02-17 19:33:34 +0200

    added about us, services, news + facebook

--- scripts/sliders.js
+++ scripts/sliders.js
@@ -1,6 +1,6 @@
 $('.slider').slick({
     autoplay: true,
-    // autoplaySpeed: 5000,
+    autoplaySpeed: 15000,
     arrows:true,
     prevArrow: '<div class="slickArrow slickPrev"><i class="fa fa-angle-left"></i></div>',
     nextArrow: '<div class="slickArrow slickNext"><i class="fa fa-angle-right"></i></div>',
