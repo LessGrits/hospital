c082a403c1361bf9cc430bd83fdc283810254c30
Oles <ruboken@gmail.com>
2020-02-19 01:03:54 +0200

    redesign add top header

--- scripts/sliders.js
+++ scripts/sliders.js
@@ -2,6 +2,6 @@ $('.slider').slick({
     autoplay: true,
     autoplaySpeed: 15000,
     arrows:true,
-    prevArrow: '<div class="slickArrow slickPrev"><i class="fa fa-angle-left"></i></div>',
-    nextArrow: '<div class="slickArrow slickNext"><i class="fa fa-angle-right"></i></div>',
+    prevArrow: '<div class="slickArrow slickPrev"><i class="im im-angle-left-circle"></i></div>',
+    nextArrow: '<div class="slickArrow slickNext"><i class="im im-angle-right-circle"></i></div>',
 });
