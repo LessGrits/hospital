03b0e0516b18149a0f1ff85d0a1a4c89dde831e8
Oles <ruboken@gmail.com>
2020-02-29 21:08:35 +0200

    add modal window, change validation

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -3,8 +3,8 @@ const contactInfo = document.querySelector('.contact-info');
 const menuBtn = document.querySelector('.menu__item_btn');
 const menuItems = document.querySelectorAll('.menu__item');
 
-menuBtn.addEventListener('click',()=>{
-    for(let item of menuItems){
+menuBtn.addEventListener('click', () => {
+    for (let item of menuItems) {
         item.classList.toggle('hide');
     }
     menuItems.classList.toggle('hide');
@@ -12,6 +12,7 @@ menuBtn.addEventListener('click',()=>{
 
 
 var map;
+
 function initialize() {
     var mapOptions = {
         zoom: 18,
@@ -22,14 +23,14 @@ function initialize() {
 
     var marker = new google.maps.Marker({
 
-        position: {lat: 49.235429, lng:  28.414315},
+        position: {lat: 49.235429, lng: 28.414315},
         map: map
     });
     var infowindow = new google.maps.InfoWindow({
         content: '<p>Marker Location:' + marker.getPosition() + '</p>'
     });
 
-    google.maps.event.addListener(marker, 'click', function() {
+    google.maps.event.addListener(marker, 'click', function () {
         infowindow.open(map, marker);
     });
 }
@@ -37,17 +38,19 @@ function initialize() {
 google.maps.event.addDomListener(window, 'load', initialize);
 
 
-addEventListener("scroll", ()=> {
+addEventListener("scroll", () => {
     const scrolled = document.scrollingElement.scrollTop;
     const position = menu.offsetTop;
-    scrolled >= position  && (menu.classList.add('fixed') , contactInfo.classList.add('hidden'));
+    scrolled >= position && (menu.classList.add('fixed') , contactInfo.classList.add('hidden'));
     scrolled <= 40 && (menu.classList.remove('fixed') , contactInfo.classList.remove('hidden'));
 });
 
 
 let phoneInput = document.querySelector('.phone')
-phoneInput.addEventListener('keydown', function(event) {
-    if( !(event.key == 'ArrowLeft' || event.key == 'ArrowRight' || event.key == 'Backspace' || event.key == 'Tab')) { event.preventDefault() }
+phoneInput.addEventListener('keydown', function (event) {
+    if (!(event.key == 'ArrowLeft' || event.key == 'ArrowRight' || event.key == 'Backspace' || event.key == 'Tab')) {
+        event.preventDefault()
+    }
     const mask = '+38 (111) 111-11-11'; // Задаем маску
 
     if (/[0-9\+\ \-\(\)]/.test(event.key)) {
@@ -59,7 +62,7 @@ phoneInput.addEventListener('keydown', function(event) {
             if (mask[currentLength] == '1') {
                 this.value = currentString + event.key;
             } else {
-                for (var i=currentLength; i<mask.length; i++) {
+                for (var i = currentLength; i < mask.length; i++) {
                     if (mask[i] == '1') {
                         this.value = currentString + event.key;
                         break;
@@ -71,6 +74,53 @@ phoneInput.addEventListener('keydown', function(event) {
     }
 });
 
+
+const submitForm = document.getElementById('submitForm');
+const modalWindow = document.getElementById('modalWindow');
+const modalContent = document.querySelector('.modal-content');
+const form = document.getElementById('form');
+
+
+submitForm.onclick = () => {
+
+    // console.log(form.elements[0].value.length >= 5);
+    // console.log(form.elements[1].value.length === 13);
+    // console.log(form.elements[2].value.search(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)>=0);
+
+    if(form.elements[0].value.length >= 5 && form.elements[1].value.length >= 13 &&form.elements[2].value.search(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)>=0){
+
+        modalWindow.classList.add('show');
+        Array.from(form.elements).map((input) => input.value = '');
+    }else{
+
+        modalWindow.classList.add('show');
+        modalContent.innerText = "Некоректне введення";
+        modalContent.classList.add("error");
+
+    }
+
+   // if(Array.from(form.elements).every((input) => {
+   //     console.log(input.checkValidity());
+   //     return input.checkValidity();
+   // }))  {
+   //     modalWindow.classList.add('show');
+   //     Array.from(form.elements).map((input) => input.value = '');
+   // }else{
+   //     modalWindow.classList.add('show');
+   //     modalContent.classList.add("error");
+   //     modalContent.innerText = "некоректне введення"
+   // }
+
+};
+
+
+
+window.onclick = (e) => {
+    e.target === modalWindow && modalWindow.classList.remove('show');
+}
+
+
+;
 
 
 
